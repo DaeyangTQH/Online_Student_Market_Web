6c10b548797e0223c680c171f8aa94fa5d61ea11
Haichan <160374468+DaeyangTQH@users.noreply.github.com>
2025-06-10 21:59:52 +0700

    Create DBcontext to connect with databse
    Kết nối thành công với database(Cần thêm thư viện, đã để sẵn trong lib)

---
+++ Online_Student_Market_Web/src/DAO/DBcontext.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author Haichann
+ */
+public class DBcontext {
+
+    protected static Connection connection;
+
+    public DBcontext() {
+        try {
+            String url = "jdbc:sqlserver://prj301-gr17.database.windows.net:1433;"
+                    + "database=OSMW_WEB;"
+                    + "encrypt=true;"
+                    + "trustServerCertificate=false;"
+                    + "hostNameInCertificate=*.database.windows.net;"
+                    + "loginTimeout=30;";
+            String user = "haitran";
+            String pass = "Group17prj@";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, user, pass);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
